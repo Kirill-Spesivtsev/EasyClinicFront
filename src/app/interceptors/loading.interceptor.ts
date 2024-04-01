5b84088c8a4d3c53c8e0fbf6cdae90df930d62e9
Kirill-Spesivtsev <ksby8819@gmail.com>
2024-04-01 18:14:58 +0300

    feat: add and configure loading interceptor

---
+++ src/app/interceptors/loading.interceptor.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable, delay, finalize } from 'rxjs';
+import { LoadingService } from '../services/loading.service';
+
+@Injectable()
+export class LoadingInterceptor implements HttpInterceptor {
+
+  constructor(private loadingService: LoadingService) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    if (request.url.includes("account/email-exists") 
+        || request.url.includes("account/username-exists")
+        || request.url.includes("account/get-current-user")){
+      return next.handle(request);
+    }
+
+    this.loadingService.startLoading();
+    
+    return next.handle(request).pipe(
+      finalize(() => this.loadingService.endLoading())
+    );
+  }
+}
